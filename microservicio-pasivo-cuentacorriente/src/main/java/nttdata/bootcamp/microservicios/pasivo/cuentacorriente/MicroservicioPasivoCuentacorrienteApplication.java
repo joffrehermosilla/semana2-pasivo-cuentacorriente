0248b633df2ecdd1db8ea7008a6a0cef52009d71
Joffre Hermosilla Salas <jhermoss@emeal.nttdata.com>
2022-04-17 11:25:44 -0500

    primer commit

---
+++ microservicio-pasivo-cuentacorriente/src/main/java/nttdata/bootcamp/microservicios/pasivo/cuentacorriente/MicroservicioPasivoCuentacorrienteApplication.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pasivo.cuentacorriente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioPasivoCuentacorrienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioPasivoCuentacorrienteApplication.class, args);
+	}
+
+}
---
+++ microservicio-pasivo-cuentacorriente/src/test/java/nttdata/bootcamp/microservicios/pasivo/cuentacorriente/MicroservicioPasivoCuentacorrienteApplicationTests.java
@@ -0,0 +1,13 @@
+package nttdata.bootcamp.microservicios.pasivo.cuentacorriente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioPasivoCuentacorrienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
